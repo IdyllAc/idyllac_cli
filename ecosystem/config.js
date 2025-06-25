80c9b09cfb8636a601045210fecac9136f1e8c40
Idyllac <victor.via7@gmail.com>
2025-06-25 21:28:20 +0200

    eco changes

---
+++ ecosystem/config.js
@@ -0,0 +1,20 @@
+// ecosystem.config.js
+module.exports = {
+    apps: [
+      {
+        name: "web-server",
+        script: "server.js",
+        env: {
+          PORT: 3000
+        }
+      },
+      {
+        name: "api-server",
+        script: "app.js",
+        env: {
+          PORT: 3001
+        }
+      }
+    ]
+  }
+  
